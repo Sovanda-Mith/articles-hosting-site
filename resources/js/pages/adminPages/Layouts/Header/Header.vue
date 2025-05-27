ff9eae376a3d2d0f473cf6a5ef2249496358d40a
Korb Daven <korbdaven@gmail.com>
2025-05-27 10:00:26 +0700

    FixedUser UI

--- resources/js/pages/adminPages/Layouts/Header/Header.vue
+++ resources/js/pages/adminPages/Layouts/Header/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full h-full pr-5 pl-5 flex justify-between items-center">
+    <div class="w-full h-16 pr-5 pl-5 flex justify-between items-center">
         <div class="flex justify-center items-center">
             <img src="../../../../assets//settingsPage_img/logo.webp" alt="" class="w-8 h-8 rounded-lg object-cover border-[1.5px] border-yellow-600">
             <span class="text-lg font-medium ml-2">Bloggist</span>
