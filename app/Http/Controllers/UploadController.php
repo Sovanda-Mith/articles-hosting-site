6985fe655de6f3a32ce8c2782b1fb90f9649b8d5
sovanda_m <vanda.can.read@gmail.com>
2025-06-19 16:50:01 +0700

    profile information settings

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -29,6 +29,28 @@ class UploadController extends Controller
     }
 
 
+    public function uploadProfileImage(Request $request)
+    {
+        $request->validate([
+          'avatar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $image = $request->file('avatar');
+        $path = 'pf_image/' . uniqid() . '.' . $image->getClientOriginalExtension();
+
+        $stored = Storage::disk('minio')->put($path, file_get_contents($image));
+
+        if (!$stored) {
+            return response()->json(['message' => 'Failed to upload image'], 500);
+        }
+
+        // Get the public URL
+        $url = Storage::disk('minio')->url($path);
+
+        return response()->json(['url' => $url]);
+    }
+
+
 
 
 
